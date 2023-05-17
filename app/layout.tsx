f52dbae9d4c24a1792d6c04486fe57082b730b6a
Yusuf Güneş <gunesyusuf838@gmail.com>
2023-05-17 12:14:26 +0300

    MDiary

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,23 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+      <meta property="og:image" content="https://www.askanonym.com/api/og" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="askanonym.com" />
+      <meta property="twitter:url" content="https://www.askanonym.com" />
+      <meta name="twitter:title" content="AskAnonym.com - Anonym to You!" />
+      <meta
+        name="twitter:description"
+        content="You can ask question anonymously or register and create a profile for getting questions!"
+      />
+      <meta
+        name="twitter:image"
+        content="https://www.askanonym.com/api/og"
+      ></meta>
+
+      <head />
         <body>
           <SupabaseProvider session={supabase}>
             <ThemeProvider attribute='class'>
